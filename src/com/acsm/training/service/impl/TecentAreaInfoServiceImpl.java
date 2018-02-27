86e3a0e519bfa69603746088335e18d76f60483e
梁临强 <L_L_Q1208@163.com>
2018-02-27 19:02:20 +0800

    新版

---
+++ src/com/acsm/training/service/impl/TecentAreaInfoServiceImpl.java
@@ -0,0 +1,43 @@
+package com.acsm.training.service.impl;/**
+ * Created by lq on 2018/2/26.
+ */
+
+import com.acsm.training.dao.TecentAreaInfoDao;
+import com.acsm.training.model.TecentAreaInfo;
+import com.acsm.training.service.TecentAreaInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Author lianglinqiang
+ * @create 2018-02-26
+ */
+@Service("tecentAreaInfoService")
+public class TecentAreaInfoServiceImpl implements TecentAreaInfoService{
+    @Autowired
+    TecentAreaInfoDao tecentAreaInfoDao;
+
+    @Override
+    public List<TecentAreaInfo> queryProvinceList() {
+        return tecentAreaInfoDao.queryProvinceList();
+    }
+
+    @Override
+    public List<TecentAreaInfo> queryCityList(int proviceAreaId) {
+        return tecentAreaInfoDao.queryCityList(proviceAreaId);
+    }
+
+    @Override
+    public List<TecentAreaInfo> queryAreaList(int cityAreaId) {
+        return tecentAreaInfoDao.queryAreaList(cityAreaId);
+    }
+
+    @Override
+    public TecentAreaInfo queryByAreaId(int areaId) {
+        return null;
+    }
+
+
+}
